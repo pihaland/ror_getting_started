d3dd92570b4d74596d0a97e206c62e1b918e1696
Andrii Pihal <pihaland@fit.cvut.cz>
2025-06-17 00:43:33 +0200

    feat: add a new validation for an Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,10 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  validates :body, length: { minimum: 500 },  if: :is_research?
+
+  def is_research?
+    %w[research thesis].map { |term| title.to_s.downcase.include?(term) }.any?
+  end
 end
