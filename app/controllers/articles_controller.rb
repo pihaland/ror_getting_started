1813fada67c787911ee858c5a211cee0847c0fdd
Andrii Pihal <pihaland@fit.cvut.cz>
2025-06-19 22:30:51 +0200

    feat: add multiple response formats (HTML, XML, JSON) to articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render xml: @articles }
+      format.json { render json: @articles }
+    end
   end
 
   def show
